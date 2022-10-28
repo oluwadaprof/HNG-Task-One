0ede75c9dcca11102e691378e17a401e2c6717ef
oluwadaprof <adeekotobiloba8@gmail.com>
2022-10-28 14:33:13 +0100

    done

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -6,8 +6,10 @@ import share from "../../assets/share.png";
 const Profile = () => {
   return (
     <div className="profile">
-      <img className="profile__img" id="profile__img" src={tobi} alt="Tobi" />
-      <p id="twitter">@oluwadaprof</p>
+      <div>
+        <img className="profile__img" id="profile__img" src={tobi} alt="Tobi" />
+        <p id="twitter">@oluwadaprof</p>
+      </div>
       <img className="share" id="profile__img" src={share} alt="share__btn" />
       <img className="dot" id="profile__img" src={dot} alt="dot__btn" />
     </div>
